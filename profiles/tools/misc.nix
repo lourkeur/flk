c3a727e37278e067f46000f6ea2a22df30e74ceb
Louis Bettens <louis@bettens.info>
2021-07-17 15:08:29 +0200

    remove binfmt

--- profiles/tools/misc.nix
+++ profiles/tools/misc.nix
@@ -1,8 +1,6 @@
 { lib, config, pkgs, ... }:
 
 {
-  boot.binfmt.emulatedSystems = lib.lists.remove pkgs.system [ "aarch64-linux" ];
-
   environment = {
     systemPackages = with pkgs; [
       ffmpeg-full
